e9f573d13684289d6819520d6170bf8ba242ccbd
Jônathas Borges <jonathas.m.borges@aluno.senai.br>
2026-08-04 15:59:26 -0300

    Implementa logica de busca e exclusão no componente Consulta

--- src/app/paginas/consulta/consulta.ts
+++ src/app/paginas/consulta/consulta.ts
@@ -1,9 +1,34 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import { RouterLink } from '@angular/router';
+import { ClienteService } from '../../servicos/cliente';
+import { Cliente } from '../../modelos/cliente';
 
 @Component({
   selector: 'app-consulta',
-  imports: [],
+  imports: [CommonModule, FormsModule, RouterLink],
   templateUrl: './consulta.html',
-  styleUrl: './consulta.css',
+  styleUrl: './consulta.css'
 })
-export class Consulta {}
+export class Consulta implements OnInit {
+  clientes: Cliente[] = [];
+  termoBusca: string = '';
+
+  constructor(private clienteService: ClienteService) {}
+
+  ngOnInit(): void {
+    this.carregarClientes();
+  }
+
+  carregarClientes(): void {
+    this.clientes = this.clienteService.pesquisar(this.termoBusca);
+  }
+
+  excluir(id?: string): void {
+    if (id && confirm('Deseja realmente excluir este cliente?')) {
+      this.clienteService.excluir(id);
+      this.carregarClientes();
+    }
+  }
+}
