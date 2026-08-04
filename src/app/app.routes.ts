d86472f28fc7598c06183704d6472f2569619d7d
Jônathas Borges <jonathas.m.borges@aluno.senai.br>
2026-08-04 14:56:24 -0300

    Configura rotas de navegação entre cadastro e consulta

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,10 @@
 import { Routes } from '@angular/router';
+import { Cadastro } from './paginas/cadastro/cadastro';
+import { Consulta } from './paginas/consulta/consulta';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', redirectTo: 'consulta', pathMatch: 'full' },
+  { path: 'cadastro', component: Cadastro },
+  { path: 'editar/:id', component: Cadastro },
+  { path: 'consulta', component: Consulta }
+];
