65cdcb654c7e2f353bde8184d64b8bb9a62ff522
Jônathas Borges <jonathas.m.borges@aluno.senai.br>
2026-08-04 14:33:27 -0300

    Define a interface Cliente com os campos obrigatorios do sistema

--- src/app/modelos/cliente.ts
+++ src/app/modelos/cliente.ts
@@ -1,1 +1,9 @@
-export interface Cliente {}
+export interface Cliente {
+  id?: string;
+  nome: string;
+  email: string;
+  cpf: string;
+  dataNascimento: string;
+  uf: string;
+  municipio: string;
+}
