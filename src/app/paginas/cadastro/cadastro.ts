363afc975a36c1030bd74d8734309f931188e02a
Jônathas Borges <jonathas.m.borges@aluno.senai.br>
2026-08-04 15:22:16 -0300

    Adiciona navegação principal e implementa logica da pagina de cadastro

--- src/app/paginas/cadastro/cadastro.ts
+++ src/app/paginas/cadastro/cadastro.ts
@@ -1,9 +1,47 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import { Router, ActivatedRoute, RouterLink } from '@angular/router';
+import { ClienteService } from '../../servicos/cliente';
+import { Cliente } from '../../modelos/cliente';
 
 @Component({
   selector: 'app-cadastro',
-  imports: [],
+  imports: [CommonModule, FormsModule, RouterLink],
   templateUrl: './cadastro.html',
-  styleUrl: './cadastro.css',
+  styleUrl: './cadastro.css'
 })
-export class Cadastro {}
+export class Cadastro implements OnInit {
+  cliente: Cliente = {
+    nome: '',
+    email: '',
+    cpf: '',
+    dataNascimento: '',
+    uf: '',
+    municipio: ''
+  };
+
+  modoEdicao = false;
+
+  constructor(
+    private clienteService: ClienteService,
+    private router: Router,
+    private route: ActivatedRoute
+  ) {}
+
+  ngOnInit(): void {
+    const id = this.route.snapshot.paramMap.get('id');
+    if (id) {
+      const clienteEncontrado = this.clienteService.obterPorId(id);
+      if (clienteEncontrado) {
+        this.cliente = { ...clienteEncontrado };
+        this.modoEdicao = true;
+      }
+    }
+  }
+
+  salvar(): void {
+    this.clienteService.salvar(this.cliente);
+    this.router.navigate(['/consulta']);
+  }
+}
