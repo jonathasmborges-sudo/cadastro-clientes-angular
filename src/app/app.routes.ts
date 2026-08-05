20f029cbd58980bc5a8099ea16e0395525f8fd6a
Jônathas Borges <jonathas.m.borges@aluno.senai.br>
2026-08-05 15:43:13 -0300

    Adiciona rota para Home e define como pagina inicial padrão

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,11 @@
 import { Routes } from '@angular/router';
+import { Home } from './paginas/home/home';
 import { Cadastro } from './paginas/cadastro/cadastro';
 import { Consulta } from './paginas/consulta/consulta';
 
 export const routes: Routes = [
-  { path: '', redirectTo: 'consulta', pathMatch: 'full' },
+  { path: '', redirectTo: 'home', pathMatch: 'full' },
+  { path: 'home', component: Home },
   { path: 'cadastro', component: Cadastro },
   { path: 'editar/:id', component: Cadastro },
   { path: 'consulta', component: Consulta }
