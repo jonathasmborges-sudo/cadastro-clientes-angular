62f4ef51eab08a02122b740ace2c14796bcef238
Jônathas Borges <jonathas.m.borges@aluno.senai.br>
2026-08-06 13:31:26 -0300

    Integra API do IBGE e aplicação de mascara no componente Cadastro

--- src/app/paginas/cadastro/cadastro.ts
+++ src/app/paginas/cadastro/cadastro.ts
@@ -3,7 +3,9 @@ import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { Router, ActivatedRoute, RouterLink } from '@angular/router';
 import { PessoaService } from '../../service/pessoa-service';
-import { Pessoa } from '../../models/pessoa';
+import { IbgeService, EstadoIBGE, MunicipioIBGE } from '../../service/ibge-service';
+import { Pessoa } from '../../model/pessoa';
+import { formatarCPF } from '../../utils/formatters';
 
 @Component({
   selector: 'app-cadastro',
@@ -22,22 +24,55 @@ export class Cadastro implements OnInit {
   };
 
   modoEdicao = false;
+  estados: EstadoIBGE[] = [];
+  municipios: MunicipioIBGE[] = [];
 
   constructor(
     private pessoaService: PessoaService,
+    private ibgeService: IbgeService,
     private router: Router,
     private route: ActivatedRoute
   ) {}
 
   ngOnInit(): void {
+    this.carregarEstados();
+
     const id = this.route.snapshot.paramMap.get('id');
     if (id) {
       const clienteEncontrado = this.pessoaService.obterPorId(id);
       if (clienteEncontrado) {
         this.cliente = { ...clienteEncontrado };
         this.modoEdicao = true;
+        if (this.cliente.uf) {
+          this.carregarMunicipios(this.cliente.uf);
+        }
       }
     }
+  }
+
+  carregarEstados(): void {
+    this.ibgeService.listarEstados().subscribe(dados => {
+      this.estados = dados;
+    });
+  }
+
+  aoMudarUf(): void {
+    if (this.cliente.uf) {
+      this.carregarMunicipios(this.cliente.uf);
+    } else {
+      this.municipios = [];
+    }
+    this.cliente.municipio = '';
+  }
+
+  carregarMunicipios(uf: string): void {
+    this.ibgeService.listarMunicipios(uf).subscribe(dados => {
+      this.municipios = dados;
+    });
+  }
+
+  aoMudarCpf(): void {
+    this.cliente.cpf = formatarCPF(this.cliente.cpf);
   }
 
   salvar(): void {
