6fd1c21220eeeb82e3649fd1909a6991f49d8b88
Jônathas Borges <jonathas.m.borges@aluno.senai.br>
2026-08-06 13:43:33 -0300

    Corrige caminho de importação do modelo Pessoa na consulta

--- src/app/paginas/consulta/consulta.ts
+++ src/app/paginas/consulta/consulta.ts
@@ -3,7 +3,7 @@ import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { RouterLink } from '@angular/router';
 import { PessoaService } from '../../service/pessoa-service';
-import { Pessoa } from '../../model/pessoa';
+import { Pessoa } from '../../models/pessoa';
 import { formatarCPF, formatarDataBR } from '../../utils/formatters';
 
 @Component({
